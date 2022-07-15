2b436f7052eddeb6edc6e734879fca0329c5ff87
nataliazo <natalya.zosimova@gmail.com>
2022-07-15 00:12:01 -0400

    variables commit

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -22,7 +22,7 @@ describe("Verifying veriables, cypress commands and jquery commands", () => {
         cy.get("a[href*='product/category&path=']").contains("Skincare").click();
         })
 
-    it.only("Navigated to specific product pages", () =>{
+    it("Navigated to specific product pages", () =>{
         cy.visit("https://automationteststore.com/");
             
         cy.get("a[href*='product/category&path=']").contains("Makeup") .click();
@@ -36,6 +36,31 @@ describe("Verifying veriables, cypress commands and jquery commands", () => {
             cy.log("Found header text: " + headerText)
             expect(headerText).is.eq("Makeup")
         })
+        
+    })
+
+    it.only("Validate properties of Contact Us page", () =>{
+        cy.visit("https://automationteststore.com/index.php?rt=content/contact");
+        
+        //Uses cypress commend and chaining
+        cy.contains('#ContactUsFrm',"Contact Us Form").find('#field_11').should('contain', "First name:")
+
+        //Jquery Approach
+        cy.contains('#ContactUsFrm',"Contact Us Form").then(text => {
+            const firstNameText = text.find('#field_11').text()
+            expect(firstNameText).to.contain('First name')
+            
+            //Embeded commands (Closure)
+            cy.get('#field_11').then(fnText=> {
+                cy.log(fnText())
+                cy.log(fnText)
+            })
+
+        })
+
+
+        
+        
         
     })
         
