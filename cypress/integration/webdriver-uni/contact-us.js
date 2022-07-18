d6cb35891efc2ccc95e5802a1ae3782355d578b8
nataliazo <natalya.zosimova@gmail.com>
2022-07-18 11:30:12 -0400

    Child window

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -5,7 +5,9 @@
 describe("Test Contact US form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () =>{
         
-        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.visit('http://www.webdriveruniversity.com')
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true}); //to not open another tab, needs to remove target _blanl
         cy.url().should('include',"contactus.html")
         cy.document().should('have.property', 'charset').and('eq','UTF-8');
         cy.title().should('include',"WebDriver | Contact Us")
@@ -18,16 +20,18 @@ describe("Test Contact US form via WebdriverUni", () => {
         cy.get('h1').should('have.text',"Thank You for your Message!")
      
               
-    })
+    });
+
     it("Should not be able to submit a successful submission via contact us form as all fields are requared", () =>{
-        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
         //cy.get("#contact-us").click({force: true});
-        
+        cy.visit('http://www.webdriveruniversity.com')
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({force:true});
         cy.get('[name="first_name"]').type("Nata");
         cy.get('[name="last_name"]').type("Zo");       
         cy.get('textarea.feedback-input').type("Hello, Cypress");
         cy.get('[type="submit"]').click();    
         cy.get('body').contains("Error: all fields are required");
-    })
+    });
         
-})
+});
